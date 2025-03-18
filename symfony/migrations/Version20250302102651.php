ff0b1f2f8dbaca793416b6f90feb1ca6e5aaae06
Vovsun03 <157281116+Vovsun03@users.noreply.github.com>
2025-03-18 16:06:03 +1000

    Ver3
    migration changed

--- symfony/migrations/Version20250302102651.php
+++ symfony/migrations/Version20250302102651.php
@@ -7,9 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
+
 final class Version20250302102651 extends AbstractMigration
 {
     public function getDescription(): string
@@ -19,32 +17,23 @@ final class Version20250302102651 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE IF EXISTS task');
-        $this->addSql('DROP TABLE IF EXISTS project');
-        $this->addSql('DROP TABLE IF EXISTS project_group');
-        $this->addSql('CREATE TABLE project_group (id UUID NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT NOW(), updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN project_group.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN project_group.created_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('CREATE TABLE project (id UUID NOT NULL, project_group_id UUID NOT NULL, name VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT NOW(), updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN project.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN project.project_group_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN project.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE project ADD project_group_id UUID NOT NULL');
         $this->addSql('CREATE INDEX IDX_2FB3D0EE1C5A2B2 ON project (project_group_id)');
-        $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EE1C5A2B2 FOREIGN KEY (project_group_id) REFERENCES project_group (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('CREATE TABLE task (id UUID NOT NULL, project_id UUID NOT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(2048) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT NOW(), updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('COMMENT ON COLUMN task.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN task.project_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN task.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EE1C5A2B2 FOREIGN KEY (project_group_id) REFERENCES project_group (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE task ADD project_id UUID NOT NULL');
         $this->addSql('CREATE INDEX IDX_527EDB25166D1F9C ON task (project_id)');
-        $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB25166D1F9C FOREIGN KEY (project_id) REFERENCES project (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE task ADD CONSTRAINT FK_527EDB25166D1F9C FOREIGN KEY (project_id) REFERENCES project (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        
     }
 
     public function down(Schema $schema): void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE task');
-        $this->addSql('DROP TABLE project');
-        $this->addSql('DROP TABLE project_group');
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE task DROP CONSTRAINT FK_527EDB25166D1F9C');
+        $this->addSql('DROP INDEX IDX_527EDB25166D1F9C');
+        $this->addSql('ALTER TABLE task DROP project_id');
+        $this->addSql('ALTER TABLE project DROP CONSTRAINT FK_2FB3D0EE1C5A2B2');
+        $this->addSql('DROP INDEX IDX_2FB3D0EE1C5A2B2');
+        $this->addSql('ALTER TABLE project DROP project_group_id');
     }
 }
